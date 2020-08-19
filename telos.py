aa7bd8ba71b7395b793e19234da9abb0e2b56b9a
Hamza Qureshi <hq343@nyu.edu>
2020-08-18 23:16:14 -0400

    Add tests for telos freedom querying/updating

--- telos.py
+++ telos.py
@@ -4,28 +4,28 @@ import pprint
 # Do you ever need to know this
 TICK_INTERVAL = 0.6
 
-FREEDOM_CD_1000 = 10//0.6
-FREEDOM_CD_250 = 20//0.6
-FREEDOM_CD_150 = 30//0.6
+FREEDOM_CD_1000 = 16
+FREEDOM_CD_250 = 31
+FREEDOM_CD_150 = 50
+FREEDOM_CD_0 = -1
 
 # telos can be stunned and bound and stuff same as the player so should probably
 # inherit from some kind of parent Stunnable class but oh well
 class Telos:
-    def __init__(self, logger, enrage=0):
+    def __init__(self, enrage, target=None):
         self.enrage = enrage
-        # Need a receiver for actions to know when this guy freedoms and attacks and stuff
-        # Probably want it to be some kind of testable object; use a logger for now
-        self.logger = logger
+        # TODO: Actually use the target
+        self.target = target
 
         # State objects to know what Telos is going to do #
         #
         # Cooldowns
         self.attack_cd_ticks = 4
-        self.freedom_cd_ticks = self.freedom_cooldown(enrage)
+        self.freedom_cd_ticks = 0 
         #
         # Status Effects
-        self.is_bound = False
-        self.is_stunned = False
+        self.bound_length_ticks = 0 
+        self.stunned_length_ticks = 0
 
     @staticmethod
     def freedom_cooldown(enrage):
@@ -35,16 +35,25 @@ class Telos:
             return FREEDOM_CD_250
         if enrage >= 150:
             return FREEDOM_CD_150
-        return -1
+        # 0 enrage can't use freedom
+        return FREEDOM_CD_0
 
     def should_freedom(self):
-        return self.is_bound or self.is_stunned
+        return self.bound_length_ticks or self.stunned_length_ticks
 
     def can_freedom(self):
-        return (self.freedom_cooldown() is not None) and (self.freedom_cd_ticks <= 0)
+        return (self.freedom_cooldown(self.enrage) > FREEDOM_CD_0) and (self.freedom_cd_ticks <= 0)
+
+    def use_freedom(self):
+        if not self.can_freedom():
+            raise RuntimeError(
+                f"Telos object at enrage '{self.enrage}' with freedom cooldown \
+                '{self.freedom_cd_ticks}' ticks cannot use freedom"
+                )
+        self.freedom_cd_ticks = self.freedom_cooldown(self.enrage)
 
     def update(self):
         if self.can_freedom() and self.should_freedom():
-            self.freedom_cd_ticks = self.freedom_cooldown()
-        
-
+            self.use_freedom()
+        if self.can_attack():
+            self.attack(target)
--- telos_test.py
+++ telos_test.py
@@ -15,3 +15,46 @@ def test_freedom_cooldown():
     ]
     for enrage, expected_freedom_cooldown in test_cases:
         assert telos.Telos.freedom_cooldown(enrage) == expected_freedom_cooldown
+
+def test_should_freedom():
+    t = telos.Telos(0)
+    assert not t.should_freedom()
+
+    t.stunned_length_ticks = 10
+    assert t.should_freedom()
+
+    for enrage in [150, 250, 1000]:
+        t.enrage = enrage
+        assert t.should_freedom()
+    
+    t.bound_length_ticks = 1
+    assert t.should_freedom()
+
+    t.bound_length_ticks = 0
+    t.stunned_length_ticks = 0
+    assert not t.should_freedom()
+
+    t.freedom_cd_ticks = 10
+    assert not t.should_freedom()
+
+    t.bound_length_ticks = 1
+    assert t.should_freedom()
+
+def test_can_freedom():
+    telos0 = telos.Telos(0)
+    assert not telos0.can_freedom()
+
+    telos150 = telos.Telos(150)
+    assert telos150.can_freedom()
+    telos150.use_freedom()
+    assert not telos150.can_freedom()
+
+    telos250 = telos.Telos(250)
+    assert telos250.can_freedom()
+    telos250.use_freedom()
+    assert not telos250.can_freedom()
+
+    telos1000 = telos.Telos(1000)
+    assert telos1000.can_freedom()
+    telos1000.use_freedom()
+    assert not telos1000.can_freedom()
